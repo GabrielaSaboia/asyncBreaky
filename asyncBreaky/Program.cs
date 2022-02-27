f288f3f227ca56c2eca8cc239e7942b1b02b70db
gabrielasaboia <gs525@njit.edu>
2022-02-26 20:20:46 -0500

    Writting the FryBacon function

--- asyncBreaky/Program.cs
+++ asyncBreaky/Program.cs
@@ -1,3 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//code taken from https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/
 
-Console.WriteLine("Hello, World!");
+using System;
+using System.Threading.Tasks;
+
+namespace AsyncBreakfast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Coffee cup = PourCoffee;
+        }
+
+        private static Juice PouOJ()
+        {
+            Console.WriteLine("Pouring orange juice");
+            return new Juice();
+        }
+
+        private static void ApplyJam(Toast toast) =>
+            Console.WriteLine("Putting jam on the toast");
+
+        private static void ApplyButter(Toast toast) =>
+            Console.WriteLine("Putting butter on toast");
+
+        private static Toast ToastBread(int slices)
+        {
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("Putting a slice of bread in the toaster");
+            }
+            Console.WriteLine("Start toasting...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Remove toast from toaster");
+            return new Toast;
+        }
+
+        private static Bacon FryBacon(int slices)
+        {
+            Console.WriteLine($"putting {slices} slices of bacon in the pan");
+        }
+
+    }
+}

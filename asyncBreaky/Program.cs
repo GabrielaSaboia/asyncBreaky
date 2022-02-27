a0bcb446a287df338ae75286cb1fdd2da194364b
gabrielasaboia <gs525@njit.edu>
2022-02-26 20:45:14 -0500

    Creationg internal classes

--- asyncBreaky/Program.cs
+++ asyncBreaky/Program.cs
@@ -1,6 +1,7 @@
 ﻿//code taken from https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/
 
 using System;
+using System.Globalization;
 using System.Threading.Tasks;
 
 namespace AsyncBreakfast
@@ -9,10 +10,26 @@ namespace AsyncBreakfast
     {
         static void Main(string[] args)
         {
-            //Coffee cup = PourCoffee;
+            Coffee cup = PourCoffee;
+            Console.WriteLine("coffee is ready");
+
+            Egg eggs = FryEggs(2);
+            Console.WriteLine("eggs are ready");
+
+            Bacon bacon = FryBacon(3);
+            Console.WriteLine("bacon is ready");
+            Toast toast = ToastBread(2);
+            ApplyButter(toast);
+            ApplyJam(toast);
+            Console.WriteLine("toast is ready");
+
+            Juice oj = PourOJ();
+            Console.WriteLine("oj is ready");
+            Console.WriteLine("Breaky is ready!");
+
         }
 
-        private static Juice PouOJ()
+        private static Juice PourOJ()
         {
             Console.WriteLine("Pouring orange juice");
             return new Juice();
@@ -30,10 +47,11 @@ namespace AsyncBreakfast
             {
                 Console.WriteLine("Putting a slice of bread in the toaster");
             }
+
             Console.WriteLine("Start toasting...");
             Task.Delay(3000).Wait();
             Console.WriteLine("Remove toast from toaster");
-            return new Toast;
+            return new Toast();
         }
 
         private static Bacon FryBacon(int slices)
@@ -45,7 +63,40 @@ namespace AsyncBreakfast
             {
                 Console.WriteLine("Flipping a slice of bacon...");
             }
+
+            Console.WriteLine("Cooking the second side of bacon...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Put bacon on a plate");
+            return new Bacon();
         }
 
+        private static Egg FryEggs(int howMany) //because this function returns an User Defined Egg Type.
+        {
+            Console.WriteLine("Warming the egg pan...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine($"Cracking {howMany} eggs");
+            Console.WriteLine("cooking the eggs...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Put eggs on plate");
+
+            return new Egg();
+        }
+
+        private static Coffee PourCoffee()
+        {
+            Console.WriteLine("Pouring coffee");
+            return new Coffee();
+        }
+    }
+
+    internal class Coffee
+    {
+
+    }
+
+    internal class Juice
+    {
+        
     }
 }
+    
